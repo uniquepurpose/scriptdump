fb2fad3c28c82cfd58d1ec34226e04b154338ea9
Christoph Holas <uniquepurpose@users.noreply.github.com>
2018-05-24 19:49:53 +0200

    Update run-update.sh

--- run-update.sh
+++ run-update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get autoclean && sudo apt-get autoremove && sudo apt-get update && sudo apt-get -V upgrade
+sudo apt-get update && sudo apt-get -V dist-upgrade && sudo apt-get autoremove --purge
