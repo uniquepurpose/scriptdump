b6a78a69bfe90b524a971dc5317624272ea7c24b
Christoph Holas <uniquepurpose@users.noreply.github.com>
2019-11-14 11:07:20 +0100

    Update add-user-grizzly.sh

--- add-user-grizzly.sh
+++ add-user-grizzly.sh
@@ -1,8 +1,16 @@
+!/bin/bash 
+# Create user
 sudo useradd -d /home/grizzly -m -s /bin/bash -c grizzly grizzly
+
+# Add SSH keys
 sudo mkdir /home/grizzly/.ssh
 sudo curl https://github.com/uniquepurpose.keys -o /home/grizzly/.ssh/authorized_keys
 sudo chmod -R 700 /home/grizzly/.ssh
 sudo chown -R grizzly:grizzly /home/grizzly/.ssh
+
+# Add grizzly to sudoers
 echo "grizzly ALL=NOPASSWD:ALL" | sudo tee /etc/sudoers.d/grizzly
 sudo chmod 440 /etc/sudoers.d/grizzly
+
+# Set password for grizzly
 sudo passwd grizzly
