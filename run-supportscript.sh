c544e1eb07ee3648f74516dfa2c9ea874446aca8
Christoph Holas <uniquepurpose@users.noreply.github.com>
2020-06-04 20:29:57 +0200

    Delete run-supportscript.sh

--- run-supportscript.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-# Script to download newest NXT support script, execute it and rename output file to include date and hostname.
-rm info_support.sh
-wget https://support.nexthink.com/hc/en-us/article_attachments/360001648105/info_support.sh
-chmod +x info_support.sh
-sudo ./info_support.sh
-d=$(date +%Y-%m-%d)
-mv info_nxt_support.tgz $d-$HOSTNAME.tgz
