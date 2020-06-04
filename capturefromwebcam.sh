4ce6cd2c74c88549d2baa9959df44786144afa36
Christoph Holas <uniquepurpose@users.noreply.github.com>
2020-06-04 20:28:43 +0200

    Create capturefromwebcam.sh

---
+++ capturefromwebcam.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo raspistill -v -o cam.jpg --exif GPS.GPSLongitude=47/1,8/1,139/10 --exif GPS.GPSLatitude=15/1,27/1,925/100 --exif GPS.GPSAltitude=6401/10 --exif IFD0.Copyright="Christoph Holas"
