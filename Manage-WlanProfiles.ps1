8c86c3229aad267bd6b5096a84e6308abd5edfd9
Christoph Holas <uniquepurpose@users.noreply.github.com>
2022-08-12 11:30:05 +0200

    Update Manage-WlanProfiles.ps1

--- Manage-WlanProfiles.ps1
+++ Manage-WlanProfiles.ps1
@@ -1,10 +1,11 @@
+# Source: https://community.spiceworks.com/how_to/133509-bulk-export-import-of-windows-wifi-profiles?page=2#how_to_comment_47598
+
 function Export-WLAN {
 <#
 .SYNOPSIS
 Exports all-user WLAN profiles
 .DESCRIPTION
 Exports all-user WLAN profiles to Xml-files to the specified directory using netsh.exe.
-Source: https://community.spiceworks.com/how_to/133509-bulk-export-import-of-windows-wifi-profiles?page=2#how_to_comment_47598
 .PARAMETER XmlDirectory
 Directory to export Xml configuration-files to
 .EXAMPLE
